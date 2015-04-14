dd6c0b2718647915101517149b6bd784dbd045bf
George Dan <george.r.dan@gmail.com>
2015-04-14 11:50:05 +1000

    Created CNSideBarView.swift

---
+++ Crunch/CNSideBarView.swift
@@ -0,0 +1,21 @@
+//
+//  CNSideBarView.swift
+//  Crunch
+//
+//  Created by George Dan on 14/04/2015.
+//  Copyright (c) 2015 Ninjaprawn. All rights reserved.
+//
+
+import UIKit
+
+class CNSideBarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
