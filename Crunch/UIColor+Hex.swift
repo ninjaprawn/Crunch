f4658374608340128e95c5909e6d2b593469573f
George Dan <george.r.dan@gmail.com>
2015-04-14 11:49:29 +1000

    Separated hex code from the nav controller

--- Crunch/UIColor+Hex.swift
+++ Crunch/UIColor+Hex.swift
@@ -6,4 +6,17 @@
 //  Copyright (c) 2015 Ninjaprawn. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    
+    convenience init(hex: Int) {
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+    }
+}
